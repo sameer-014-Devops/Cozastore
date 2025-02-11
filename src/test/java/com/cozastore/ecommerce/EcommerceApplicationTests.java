5f1fa497268b6b92708fb8bba273bc3501cd107f
Sameer A <sksameer4970@gmail.com>
2025-02-11 22:37:42 +0530

    Add files via upload

---
+++ src/test/java/com/cozastore/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cozastore.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
